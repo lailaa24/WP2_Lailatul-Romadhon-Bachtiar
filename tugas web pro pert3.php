1a0ee467cc0da46fb8e824b98d2dab7ff50283a3
lailaa24 <lailatul.romadhon24@gmail,com>
2022-03-22 09:08:01 +0700

    Menambahkan file project latihan

---
+++ tugas web pro pert3.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<title>Tugas Web Programing I Pert 3</title>
+</head>
+<body>
+<?php
+	$nama="Lintang Seruni Senja";
+	$alamat="Jl Badak No.2 Cimahi-Bandung";
+	$ttl="Jakarta,20 Oktober 1990";
+	$tlpn="0812345678";
+
+echo"Nama Saya : $nama<br>";
+echo"Alamat : $alamat<br>";
+echo"TTL : $ttl<br>";
+echo"Telp : $tlpn<br>";
+?>
+</body>
+</html>	
---
+++ tugas web pro pert4.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<title>Tugas Web Programing I Pert 4</title>
+</head>
+<body>
+<?php
+	$pnjng_sisi=15;
+	$volume=$pnjng_sisi*$pnjng_sisi*$pnjng_sisi;
+ 	$teks1="Belajar Menghitung";
+	$teks2=" Volume Kubus";
+	$hasil=$teks1 . $teks2;
+
+echo"$hasil<br>";
+echo"Panjang Sisi Kubus = $pnjng_sisi<br>";
+echo"Volume Kubus = $volume cm3<br>";
+?>
+</body>
+</html>
